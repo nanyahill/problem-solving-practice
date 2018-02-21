5247391e793c18b29ddcd364aa640ca6907c7488
nanya <ihunanya.u@gmail.com>
2018-02-20 16:18:04 -0800

    Added new Hash Table problems

---
+++ src/com/problems/epi/code/hash_tables/LRUCache2.java
@@ -0,0 +1,32 @@
+package com.problems.epi.code.hash_tables;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * An alternative solution to the LRU cache problem using LinkedHashMap
+ * Note: LinkedHashMap uses a doubly linked list and a hash map under the hood; just like the implementation LRUCache.java
+ */
+public class LRUCache2 {
+    int capacity;
+    LinkedHashMap<Integer, Double> map = new LinkedHashMap<Integer, Double>(capacity, 1.0f, true) {
+        @Override
+        protected boolean removeEldestEntry(Map.Entry eldest) {
+            return map.size() > capacity;
+        }
+    };
+
+    public double get(int key) {
+        if(!map.containsKey(key)) return -1;
+        return map.get(key);
+    }
+
+    public void put(int k, double v) {
+        if(!map.containsKey(k))
+            map.put(k,v);
+    }
+
+    public boolean remove(int key) {
+        return map.remove(key) != null;
+    }
+}
