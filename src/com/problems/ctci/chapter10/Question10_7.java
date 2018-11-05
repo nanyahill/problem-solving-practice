185e7c23e967c96338e82c667e6469cd747de477
nanya <ihunanya.u@gmail.com>
2018-11-05 15:23:35 -0800

    Added new problems

--- src/com/problems/ctci/chapter10/Question10_7.java
+++ src/com/problems/ctci/chapter10/Question10_7.java
@@ -2,26 +2,109 @@ package com.problems.ctci.chapter10;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.BitSet;
 import java.util.Scanner;
 
 public class Question10_7 {
 
-    public static int findMissingInt(String filename) throws FileNotFoundException {
-        if(filename == null || filename.length() == 0) return -1;
-        long number_of_ints = (long)(Integer.MAX_VALUE) + 1; // convert Integer.MAX_VALUE to long first
-        byte[] bitArray = new byte[(int) (number_of_ints / 8)]; // the paraenthese is important!
-        Scanner in = new Scanner(new FileReader(filename));
-        while(in.hasNextInt()) {
-            int num = in.nextInt();
-            bitArray[num / 8] |= (1 << num % 8);
-        }
+    // Note: BitSet is not used here because number_of_ints is a long which cannot be used to init a BitSet.
+    public static int findMissingInt_1GB_Memory(String file) {
+        long size = (long) Integer.MAX_VALUE + 1; // convert Integer.MAX_VALUE to long first
+        byte[] bitArray = new byte[(int)(size / 8)]; // the parentheses is important!
+        readFile(file, bitArray);
         for(int i = 0; i < bitArray.length; i++) {
-            int bitVector = bitArray[i];
+            byte currByte = bitArray[i];
             for(int j = 0; j < 8; j++) {
-                if((bitVector & (1 << j)) == 0)
+                if((currByte & (1 << j)) == 0) {
                     return i * 8 + j;
+                }
             }
         }
         return -1;
+    }
+
+    private static void readFile(String file, byte[] bitArray) {
+        try {
+            Scanner in = new Scanner(new FileReader(file));
+            while (in.hasNextInt()) {
+                int num = in.nextInt();
+                bitArray[num / 8] |= (1 << (num % 8));
+            }
+        }
+        catch(FileNotFoundException e) {}
+    }
+
+    /** Follow Up: What if 10MB is available and input has 1 billion distinct integers? */
+    public static int findMissingInt_10MB_Memory(String file) {
+        int number_of_buckets = 1 << 16;
+        int[] countArray = new int[number_of_buckets];
+        readFile(file, countArray);
+        for(int i = 0; i < countArray.length; i++) {
+            if(countArray[i] < number_of_buckets) {
+                BitSet bitArray = new BitSet(number_of_buckets);
+                try {
+                    Scanner in = new Scanner(new FileReader(file));
+                    while(in.hasNextInt()) {
+                        int num = in.nextInt();
+                        if(i == (num >>> 16))
+                            bitArray.set(num & (number_of_buckets - 1));
+                    }
+                }
+                catch(FileNotFoundException e) {}
+                for(int j = 0; j < bitArray.size(); j++) {
+                    if(!bitArray.get(j)) {
+                        return (i << 16) | j;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static void readFile(String filename, int[] countArray) {
+        try {
+            Scanner in = new Scanner(new FileReader(filename));
+            while(in.hasNextInt()) {
+                int num = in.nextInt();
+                countArray[num >> 16]++;
+            }
+        }
+        catch(FileNotFoundException e) {}
+    }
+
+    // From CTCI Repo
+    public static void generateFile(String filename, int max, int missing) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter(filename);
+
+        for (int i = 0; i < max && i >= 0; i++) {
+            if (i != missing) {
+                writer.println(i);
+            }
+            if (i % 10000 == 0) {
+                System.out.println("Now at location: " + i);
+            }
+        }
+        writer.flush();
+        writer.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String filename = "/Users/Nanya/software-development/CtCI-6th-Edition/Java/Ch 10. Sorting and Searching/Q10_07_Missing_Int/input.txt";
+        int max = 10000000;
+        int missing = 1234325;
+        long startTime; //start time
+        long endTime;   //end time
+        double time;    //time difference
+        System.out.println("Generating file...");
+        generateFile(filename, max, missing);
+        System.out.println("Generated file from 0 to " + max + " with " + missing + " missing.");
+        System.out.println("Searching for missing number...");
+
+        startTime = System.currentTimeMillis();
+        System.out.println("Missing value: " + findMissingInt_1GB_Memory(filename));
+        endTime = System.currentTimeMillis();
+        time = (endTime - startTime) / 1000.0;
+        System.out.println(time);
     }
 }
---
+++ src/com/problems/sandbox/KnapSackMaximumWeight.java
@@ -0,0 +1,88 @@
+package com.problems.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KnapSackMaximumWeight {
+
+    public static class Item {
+        public int weight;
+        public int value;
+
+        public Item(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
+    }
+
+    public static int findMaximumProfit_Recursive(List<com.problems.classic_problems.dynamic_programming.KnapsackProblem.Item> items, int weight) {
+        if(items == null || items.size() == 0) return 0;
+        return findMaximumProfit_Recursive(items, items.size() - 1, weight);
+    }
+
+    private static int findMaximumProfit_Recursive(List<com.problems.classic_problems.dynamic_programming.KnapsackProblem.Item> items, int i, int w) {
+        if(i == 0) return 0;
+        if(w == 0) return 0;
+        return Math.max(findMaximumProfit_Recursive(items, i - 1, w), items.get(i).weight > w ? 0 : items.get(i).value + findMaximumProfit_Recursive(items, i - 1, w - items.get(i).weight));
+    }
+
+    public static int findMaximumProfit_TopDownDP(List<com.problems.classic_problems.dynamic_programming.KnapsackProblem.Item> items, int weight) {
+        if (items == null || items.size() == 0) return 0;
+        int[][] table = new int[items.size() + 1][weight + 1];
+        for(int[] row : table)Arrays.fill(row, -1);
+        return findMaximumProfit_TopDownDP(items, items.size(), weight, table);
+    }
+
+    private static int findMaximumProfit_TopDownDP(List<com.problems.classic_problems.dynamic_programming.KnapsackProblem.Item> items, int i, int j, int[][] table) {
+        if (i == 0 || j == 0) return 0;
+        //if (j == 0) return 0;
+        if (table[i][j] == -1) {
+            table[i][j] = Math.max(findMaximumProfit_TopDownDP(items, i - 1, j, table), items.get(i-1).weight > j ? 0 : items.get(i-1).value + findMaximumProfit_TopDownDP(items, i - 1, j - items.get(i-1).weight, table));
+        }
+        return table[i][j];
+    }
+
+    public static int findMaximumProfit_BottomUpDP(List<com.problems.classic_problems.dynamic_programming.KnapsackProblem.Item> items, int weight) {
+        if(items == null || items.size() == 0) return 0;
+        int[][] table = new int[items.size() + 1][weight + 1];
+        //Arrays.fill(table[0], 0);
+        for(int i = 1; i < table.length; i++) {
+            for(int j = 0; j < table[0].length; j++) {
+                int itemWeight = items.get(i-1).weight;
+                int itemValue = items.get(i-1).value;
+                if(itemWeight > j) table[i][j] = table[i-1][j];
+                else table[i][j] = Math.max(table[i-1][j], itemValue + table[i-1][j - itemWeight]);
+            }
+        }
+        return table[items.size()][weight];
+    }
+
+    public static int findMaximumProfit_BottomUpDP_WithMinSpace(List<com.problems.classic_problems.dynamic_programming.KnapsackProblem.Item> items, int weight) {
+        if(items == null || items.size() == 0) return 0;
+        int[] table = new int [weight + 1];
+        for(int i = 1; i <= items.size(); i++) {
+            for(int j = weight; j >= 0; j--) { // this order of for loop iteration is important, because you cannot take multiple instances of an item. With unbounded, you can start from j = 1 to wgt
+                int itemWeight = items.get(i-1).weight;
+                int itemValue = items.get(i-1).value; // not needed
+                if(itemWeight > j) continue;
+                else table[j] = Math.max(table[j], itemWeight + table[j - itemWeight]);
+                }
+            }
+        return table[weight];
+    }
+
+    public static void main(String[] args) {
+        int[][] items = {{60, 5}, {50, 3}, {70, 4}, {30, 2}};
+        int[][] items2 = {{21, 2}, {31, 3}, {51, 5}, {71, 7}};
+        //[49, 12], [97, 18], [80, 46], [18, 33], [55, 4], [11, 34], [55, 72], [11, 100], [4, 84], [78, 75], [52, 96], [36, 35], [25, 60], [100, 94], [57, 87], [85, 62], [27, 72], [35, 39], [85, 82], [10, 16], [13, 13], [13, 50], [50, 43], [19, 100], [41, 26], [38, 39], [37, 73], [98, 24], [17, 26], [98, 97], [22, 14], [85, 93], [87, 59], [7, 81], [13, 54], [39, 80], [17, 86], [95, 80], [3, 17], [90, 76], [29, 2], [5, 91], [14, 59], [85, 12], [38, 50], [28, 94], [94, 84], [99, 50], [39, 86], [95, 46], [32, 2], [31, 38], [95, 64], [22, 16], [27, 9], [9, 25], [38, 95], [26, 83], [83, 8], [18, 100], [3, 31], [92, 64], [46, 47], [9, 84], [99, 39], [23, 1], [44, 87], [41, 41], [50, 35], [89, 33], [30, 87], [74, 56], [13, 55], [70, 12], [75, 100], [82, 55], [36, 34], [4, 34], [79, 59], [100, 90], [85, 11], [18, 52], [69, 3], [72, 1], [3, 72], [11, 59], [34, 5], [36, 2], [7, 34], [10, 40], [69, 34], [2, 34], [49, 52], [67, 79], [99, 33], [35, 80], [69, 10], [96, 85], [86, 97], [44, 21], [37, 56], [16, 46], [63, 36], [87, 2], [21, 69], [34, 32], [8, 15], [50, 61], [100, 69], [37, 11], [66, 98], [54, 43], [33, 35], [53, 86], [17, 33], [84, 28], [6, 34], [93, 40], [31, 20], [66, 43], [61, 3], [2, 63], [76, 31], [7, 66], [92, 13], [54, 10], [57, 16], [44, 93], [87, 55], [84, 28], [6, 19], [76, 46], [16, 75], [35, 70], [51, 34], [15, 12], [26, 6], [64, 45], [35, 73], [46, 46], [41, 2], [32, 87], [41, 6], [62, 24], [47, 16], [27, 74], [61, 100]];
+        int weight = 5;
+        List<com.problems.classic_problems.dynamic_programming.KnapsackProblem.Item> itemsList = new ArrayList<>();
+        for(int i = 0; i < items2.length; i++) {
+            itemsList.add(new com.problems.classic_problems.dynamic_programming.KnapsackProblem.Item(items2[i][1], items2[i][0]));
+        }
+        System.out.print(findMaximumProfit_BottomUpDP_WithMinSpace(itemsList, 12));
+
+    }
+}
+
